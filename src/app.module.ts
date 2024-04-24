af3a0fad268e8a1ea224a66ae65721b55fac0bfa
Ismael Soares Doria <ismaelsoaresdoria@gmail.com>
2024-04-24 17:39:14 -0300

    Add variables global env.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from "@nestjs/common";
 import { PrismaService } from "./prisma/prisma.service";
 import { CreateAccountController } from "./controllers/create-account.controller";
 import { CreateMovieController } from "./controllers/create-movie.controller";
+import { ConfigModule } from "@nestjs/config";
+import { envSchema } from "./env";
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
   controllers: [CreateAccountController, CreateMovieController],
   providers: [PrismaService],
 })
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
+
 @Injectable()
 export class PrismaService
   extends PrismaClient
@@ -10,7 +11,6 @@ export class PrismaService
       log: ["query", "error", "warn"],
     });
   }
-
   onModuleInit() {
     console.log(`Banco conectado!`);
     return this.$connect();
